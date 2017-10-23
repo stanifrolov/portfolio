55135490b178281f77bb48abb611b9984261a117
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-10-23 22:31:14 +0200

    Added navbar, Added text, Added blog, Added pictures

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,16 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from newsletter.views import home,education,work,sideprojects,blog, contact
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'newsletter.views.home', name='home'),
-    url(r'^education/', 'newsletter.views.education', name='education'),
-    url(r'^work/', 'newsletter.views.work', name='work'),
-    url(r'^sideprojects/', 'newsletter.views.sideprojects', name='sideprojects'),
-    # url(r'^blog/$', 'newsletter.views.blog', name='blog'),
-    # url(r'^blog/post1/', 'newsletter.views.post1', name='post1'),
-    # url(r'^blog/post2/', 'newsletter.views.post2', name='post2'),
+    url(r'^$', home, name='home'),
+    # url(r'^education/', education, name='education'),
+    # url(r'^work/', work, name='work'),
+    # url(r'^sideprojects/', sideprojects, name='sideprojects'),
+    url(r'^blog/$', blog, name='blog'),
+    # url(r'^contact/$', contact, name='contact'),
+    url(r'^blog/post1/', 'newsletter.views.post1', name='post1'),
+    url(r'^blog/post2/', 'newsletter.views.post2', name='post2'),
 
 ]
 
